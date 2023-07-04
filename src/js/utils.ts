2c53260ef5190ecb59ba6e2e5f5f658610085471
figgyhunter <leonardo.roi3@gmail.com>
2023-07-04 20:48:52 +0200

    new utility functions

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,13 +1,55 @@
 export function slugify(text) {
-  text = text.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
-  text = text.toLowerCase(); // convert texting to lowercase
-  text = text
-    .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
-    .replace(/\s+/g, "-") // replace spaces with hyphens
-    .replace(/-+/g, "-"); // remove consecutive hyphens
-  return text;
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
 }
 
 export function formatDate(date) {
-  return new Date(date).toLocaleDateString(undefined);
+  return new Date(date).toLocaleDateString("en-US", {
+    timeZone: "UTC",
+  });
+}
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    // filterOutDrafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  // sortByDate or randomize
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  return filteredPosts;
 }
