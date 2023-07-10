b712bded622dbf1ac765fa65d6884c0b88ef77a5
figgyhunter <leonardo.roi3@gmail.com>
2023-07-10 23:31:20 +0200

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({ serviceEntryPoint: "@astrojs/image/sharp" })],
+  base: "codeblog/",
+  site: "https://leotg.com/codeblog",
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap({ filter: (page) => page !== "https://leotg.com/about" }),
+  ],
 });
